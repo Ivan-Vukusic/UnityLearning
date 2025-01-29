1bbbf34d7a05191129c143fac6cf7c6cc17d2d81
Ivan Vukušić <ivukusic27@gmail.com>
2025-01-29 20:05:28 +0100

    Prva naredba za kretnju letjelice

---
+++ Flyer Booster/Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] InputAction thrust;
+    [SerializeField] float thrustStrength = 100f; 
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }    
+
+    private void OnEnable() 
+    {
+        thrust.Enable();        
+    }
+
+    private void FixedUpdate()
+    {
+        if (thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * thrustStrength);
+        }
+    }  
+}
