ff99832abf623bf361d84973d76dadfdd01fe1c5
Ivan Vukušić <ivukusic27@gmail.com>
2025-01-30 21:28:50 +0100

    Dodavanje rotacije letjelice

--- Flyer Booster/Assets/Scripts/Movement.cs
+++ Flyer Booster/Assets/Scripts/Movement.cs
@@ -4,7 +4,9 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
-    [SerializeField] float thrustStrength = 100f; 
+    [SerializeField] InputAction rotation; 
+    [SerializeField] float thrustStrength = 100f;
+    [SerializeField] float rotationStrength = 100f; 
     Rigidbody rb;
 
     private void Start()
@@ -14,14 +16,39 @@ public class Movement : MonoBehaviour
 
     private void OnEnable() 
     {
-        thrust.Enable();        
+        thrust.Enable();
+        rotation.Enable();        
     }
 
     private void FixedUpdate()
+    {
+        Thrust();
+        Rotation();
+    }
+
+    private void Thrust()
     {
         if (thrust.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thrustStrength);
         }
-    }  
+    }
+
+    private void Rotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if (rotationInput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+        else if (rotationInput > 0)
+        {
+            ApplyRotation(-rotationStrength);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+    }
 }
